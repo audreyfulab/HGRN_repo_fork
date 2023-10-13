72e1a4218d34f783ad1989fc5078407794564a74
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2023-10-13 13:48:17 -0700

    Update train.py
    minor fixes

--- HGRN_software/train.py
+++ HGRN_software/train.py
@@ -15,6 +15,8 @@ from torch.utils.data import DataLoader, Dataset
 import time
 import torch.optim as optimizers 
 from utilities import Modularity, BCSS, WCSS, node_clust_eval
+import matplotlib.pyplot as plt
+import seaborn as sbn
 
 #------------------------------------------------------
 #custom pytorch dataset
@@ -55,9 +57,9 @@ class ModularityLoss(nn.Module):
         
     def forward(self, all_A, all_P):
         loss = torch.Tensor([0])
-        for index, (A, P) in enumerate(zip(all_A, all_P)):
+        for index, (A,P) in enumerate(zip(all_A, all_P)):
             loss+= Modularity(A, P)
-        return loss.sum()
+        return loss
     
 #------------------------------------------------------  
 class ClusterLoss(nn.Module):
@@ -107,13 +109,17 @@ def fit(model, X, A, optimizer='Adam', batch = 128, epochs = 100, update_interva
     """
     
     """
-    train_results = []
-    test_results = []
+    #train_results = []
+    #test_results = []
     
     #train_data, test_data, train_size, test_size = train_test(X.transpose(0,1), 
     #                                                          prop_train) 
     #batched_train_data = batch_data(train_data, batch_size=batch)
     
+    loss_history=[]
+    recon_A_loss_hist=[]
+    recon_X_loss_hist=[]
+    mod_loss_hist=[]
     
     print(model)
     
@@ -156,14 +162,21 @@ def fit(model, X, A, optimizer='Adam', batch = 128, epochs = 100, update_interva
         elif comm_loss == 'Clustering':
             community_loss = community_loss_fn(X, S)
         
+        
+        X_loss = X_recon_loss(X_hat, X)
+        A_loss = A_recon_loss(A_hat, A)
         #sum up losses
-        loss = X_recon_loss(X_hat, X)+gamma*A_recon_loss(A_hat, A)-delta*community_loss
+        loss = X_loss+gamma*A_loss-delta*community_loss
 
         # update
         loss.backward()
         optimizer.step()
         total_loss += loss.cpu().item()
         #evaluate epoch
+        loss_history.append(total_loss)
+        recon_A_loss_hist.append(float(A_loss.detach().numpy()))
+        recon_X_loss_hist.append(float(X_loss.detach().numpy()))
+        mod_loss_hist.append(float(community_loss.detach().numpy()))
         if epoch % update_interval == 0:
             
             model.eval()
@@ -171,23 +184,44 @@ def fit(model, X, A, optimizer='Adam', batch = 128, epochs = 100, update_interva
             X_pred, A_pred, A_list, P_list, S_pred = model.forward(X, A)
             pred_labels = S_pred[0].detach().numpy()
             
+            #loss printing
             print('Epoch {} total_loss = {:.3f}'.format(
                 epoch, total_loss
                 ))
             
-            print('Community Detection Loss = {:.3f}, X Recontrstuction = {:.3f}, A Recontructions = {:.3f}'.format(
-                int(community_loss.detach().numpy()), 
-                X_recon_loss(X_hat, X).detach().numpy(), 
-                A_recon_loss(A_hat, A).detach().numpy()
-                ))
+            print('Community Detection Loss = {:.4f}, X Recontrstuction = {:.4f}, A Recontructions = {:.4f}'.format(
+                mod_loss_hist[-1], 
+                recon_X_loss_hist[-1], 
+                recon_A_loss_hist[-1]))
+            
+            #evaluating performance homogenity, completeness and NMI
             eval_metrics = node_clust_eval(pred_labels=pred_labels, **kwargs)
             print('Evaluations: homogeniety = {:.3f}, completeness = {:.3f}, NMI = {:.3f}'.format(
                 eval_metrics[0], eval_metrics[1], eval_metrics[2]
                 ))
             print('-' * 80)
             
+            #plotting training curves
+            if epoch >= 10:
+                fig, (ax1, ax2) = plt.subplots(2,2, figsize=(12,10))
+                #total loss
+                ax1[0].plot(range(0, epoch+1), loss_history, label = 'Total Loss')
+                ax1[0].set_xlabel('Training Epochs')
+                ax1[0].set_ylabel('Total Loss')
+                #reconstruction of graph adjacency
+                ax1[1].plot(range(0, epoch+1), recon_A_loss_hist, label = 'Graph Reconstruction Loss')
+                ax1[1].set_xlabel('Training Epochs')
+                ax1[1].set_ylabel('Graph Reconstruction Loss')
+                #reconstruction of node attributes
+                ax2[0].plot(range(0, epoch+1), recon_X_loss_hist, label = 'Attribute Reconstruction Loss')
+                ax2[0].set_xlabel('Training Epochs')
+                ax2[0].set_ylabel('Attribute Reconstruction Loss')
+                #modularity
+                ax2[1].plot(range(0, epoch+1), mod_loss_hist, label = 'Modularity Loss')
+                ax2[1].set_xlabel('Training Epochs')
+                ax2[1].set_ylabel('Modularity Loss')
     
+    
+    #return 
     X_final, A_final, A_all_final, P_all_final, S_final = model.forward(X, A)
     return X_final, A_final, A_all_final, P_all_final, S_final
-    #X_final = model.forward(X, A)
-    #return X_final
