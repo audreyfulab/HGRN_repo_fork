663a4608b7c0ed09808b7c6ffe4df5b122a3c4ae
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2024-02-06 15:40:02 -0800

    scripts to run sim scenarios

---
+++ Simulated Hierarchies/run_simulation_scripts/Run_ALL_Reso_1005_TOAL_TRUE.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 12:51:04 2024
+
+@author: Bruin
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import gc
+import numpy as np
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import sys
+sys.path.append('/mnt/ceph/jarredk/HGRN_repo/Simulated_Hierarchies/')
+sys.path.append('/mnt/ceph/jarredk/HGRN_repo/HGRN_software/')
+from simulation_utilities import post_hoc_embedding
+from run_simulations import run_simulations
+
+print('*'*80)
+print('*'*80)
+print('*'*80)
+print('Resolution = {}, Gamma = {}, TOAL = {}'.format(
+    [100, 5], 0, True
+    ))
+output = run_simulations(save_results=True,
+                         reso=[100, 5],
+                         hd=[256, 128, 64],
+                         gam=0,
+                         delt=1, 
+                         learn_rate=1e-4,
+                         epochs = 500,
+                         updates = 100,
+                         loss_fn='Modularity',
+                         activation = 'LeakyReLU',
+                         TOAL=True, 
+                         sp = '/mnt/ceph/jarredk/HGRN_repo/Simulated_Hierarchies/Simulation_Results/')
+
+del output
+gc.collect()
+
+print('*'*80)
+print('*'*80)
+print('*'*80)
+print('Resolution = {}, Gamma = {}, TOAL = {}'.format(
+    [100, 5], 0.5, True
+    ))
+output = run_simulations(save_results=True,
+                         reso=[100, 5],
+                         hd=[256, 128, 64],
+                         gam=0.5,
+                         delt=1, 
+                         learn_rate=1e-4,
+                         epochs = 500,
+                         updates = 100,
+                         loss_fn='Modularity',
+                         activation = 'LeakyReLU',
+                         TOAL=True, 
+                         sp = '/mnt/ceph/jarredk/HGRN_repo/Simulated_Hierarchies/Simulation_Results/')
+
+del output
+gc.collect()
+
+print('*'*80)
+print('*'*80)
+print('*'*80)
+print('Resolution = {}, Gamma = {}, TOAL = {}'.format(
+    [100, 5], 1, True
+    ))
+output = run_simulations(save_results=True,
+                         reso=[100, 5],
+                         hd=[256, 128, 64],
+                         gam=1,
+                         delt=1, 
+                         learn_rate=1e-4,
+                         epochs = 500,
+                         updates = 100,
+                         loss_fn='Modularity',
+                         activation = 'LeakyReLU',
+                         TOAL=True, 
+                         sp = '/mnt/ceph/jarredk/HGRN_repo/Simulated_Hierarchies/Simulation_Results/')
+
+del output
+gc.collect()
