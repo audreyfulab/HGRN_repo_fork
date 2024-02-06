updated loss function and corrected KMEAN loss calculation
Added a function called easy_renumbering which takes the assignment labels and relabels them to be 0 through number of communities k

HGRN_software/train.py
@@ -77,34 +77,35 @@ class ClusterLoss(nn.Module):
 
     
     
-    def forward(self, Attributes, cluster_labels):
+    def forward(self, Attributes, Probabilities, cluster_labels):
         
         """
         computes forward loss
         """
         loss = torch.Tensor([0])
         loss_list = []
-        for idx, (features,labels) in enumerate(zip(Attributes, cluster_labels)):
+        for idx, (features, probs, labels) in enumerate(zip(Attributes, Probabilities, cluster_labels)):
             #compute total number of clusters
-            number_of_clusters = torch.tensor(torch.unique(labels).shape[0])
+            number_of_clusters = len(torch.unique(labels))
             #within cluster sum of squares
-            within_ss, centroids = WCSS(X = Attributes, 
-                                        clustlabs=labels, 
-                                        num_clusters = number_of_clusters,
+            within_ss, centroids = WCSS(X = features,
+                                        P = probs,
+                                        S=labels, 
+                                        k = number_of_clusters,
                                         norm_degree=self.norm_deg,
                                         weight_by = self.weighting)
             #between cluster sum of squares
-            between_ss = BCSS(X = Attributes,
-                              cluster_centroids=centroids,
-                              numclusts = number_of_clusters,
-                              norm_degree = self.norm_deg, 
-                              weight_by = self.weighting)
+            # between_ss = BCSS(X = Attributes,
+            #                   cluster_centroids=centroids,
+            #                   numclusts = number_of_clusters,
+            #                   norm_degree = self.norm_deg, 
+            #                   weight_by = self.weighting)
             #add loss
-            #loss_list.append(float((between_ss/within_ss).detach().numpy()))
-            #loss += between_ss/within_ss
+            loss_list.append(float(within_ss.detach().numpy()))
+            loss += within_ss
             
-            loss_list.append(float((within_ss-between_ss).detach().numpy()))
-            loss += torch.subtract(within_ss, between_ss)
+            # loss_list.append(float((within_ss-between_ss).detach().numpy()))
+            # loss += torch.subtract(within_ss, between_ss)
 
         return loss, loss_list
     
@@ -120,7 +121,7 @@ class ClusterLoss(nn.Module):
 #------------------------------------------------------
 #this function fits the HRGNgene model to data
 def fit(model, X, A, optimizer='Adam', epochs = 100, update_interval=10, lr = 1e-4, 
-        gamma = 1, delta = 1, layer_resolutions = [1,1], comm_loss = ['Modularity', 'Clustering'], 
+        gamma = 1, delta = 1, lam = 1, layer_resolutions = [1,1], comm_loss = ['Modularity', 'Clustering'], 
         true_labels = [], save_output = False, output_path = 'path/to/output', fs = 10, 
         ns = 10, turn_off_A_loss = False, **kwargs):
     """
@@ -177,21 +178,32 @@ def fit(model, X, A, optimizer='Adam', epochs = 100, update_interval=10, lr = 1e
         
         #update all output list
         all_out.append([X_hat, A_hat, X_all, A_all, P_all, S_relab, S_all, [len(np.unique(i)) for i in S_all]])
-        #compute community detection loss
-        if comm_loss == 'Modularity':    
-            community_loss, comloss_values = community_loss_fn(A_all, P_all, layer_resolutions)
-        elif comm_loss == 'Clustering':
-            community_loss, comloss_values = community_loss_fn(X, S_all)
+        
         
         #compute reconstruction losses for graph and attributes
         X_loss = X_recon_loss(X_hat, X)
         A_loss = A_recon_loss(A_hat, A)
         
-        #compute total loss function
-        if(turn_off_A_loss == True):
-            loss = 0*A_loss+gamma*X_loss-delta*community_loss
-        else:
-            loss = A_loss+gamma*X_loss-delta*community_loss
+        
+        #compute community detection loss
+        if comm_loss == 'Modularity':    
+            community_loss, comloss_values = community_loss_fn(A_all, P_all, layer_resolutions)
+            #compute total loss function
+            if(turn_off_A_loss == True):
+                loss = 0*A_loss+gamma*X_loss-delta*community_loss
+            else:
+                loss = A_loss+gamma*X_loss-delta*community_loss
+        elif comm_loss == 'Clustering':
+            community_loss, comloss_values = community_loss_fn(X_all, P_all, S_sub)
+            #compute total loss function
+            if(turn_off_A_loss == True):
+                loss = 0*A_loss+gamma*X_loss+delta*community_loss
+            else:
+                loss = A_loss+gamma*X_loss+delta*community_loss
+        
+       
+        
+        
 
         #compute backward pass
         loss.backward()

HGRN_software/utilities.py
@@ -34,38 +34,76 @@ def Modularity(A,P,res=1):
 
 
 #This function computes the within cluster sum of squares (WCSS)
-def WCSS(X = None, clustlabs = None, num_clusters=None, norm_degree = 2,
+# def WCSS(X = None, clustlabs = None, num_clusters=None, norm_degree = 2,
+#          weight_by = ['kmeans','anova']):
+    
+#     """
+#     Within-Cluster Sum of Squares
+#     """
+    
+#     #X_tensor = torch.tensor(X, requires_grad=True)
+#     num_features = X.shape[1]
+#     num_nodes = X.shape[0]
+#     centroid_mat = torch.zeros(num_clusters, num_features)
+#     nodes = torch.arange(num_nodes)
+#     total_wcss = torch.zeros(1).float()
+#     clust_IDs = torch.unique(clustlabs)
+    
+#     for i in np.arange(num_clusters):
+#         which = nodes[clustlabs == clust_IDs[i]]
+#         clust = torch.index_select(X, 0, which)
+#         centroid_mat[i,:] = torch.mean(clust, dim = 0)
+#         pdist = nn.PairwiseDistance(p=norm_degree)
+#         if weight_by == 'kmeans':
+#             total_wcss += torch.mean(pdist(clust, centroid_mat[i,:].reshape(1, num_features)))
+#         else:
+#             total_wcss += torch.sum(pdist(clust, centroid_mat[i,:].reshape(1, num_features)))
+            
+#     if weight_by == 'kmeans':
+#         MSW = (1/num_clusters)*total_wcss
+#     else:
+#         MSW = (1/(X.shape[0]-num_clusters))*total_wcss
+            
+#     return MSW, centroid_mat
+def WCSS(X, P, S, k, norm_degree = 2,
          weight_by = ['kmeans','anova']):
     
     """
-    
+    Within-Cluster Sum of Squares
     """
     
     #X_tensor = torch.tensor(X, requires_grad=True)
-    num_features = X.shape[1]
-    num_nodes = X.shape[0]
-    centroid_mat = torch.zeros(num_clusters, num_features)
-    nodes = torch.arange(num_nodes)
-    total_wcss = torch.zeros(1).float()
-    clust_IDs = torch.unique(clustlabs)
+    Snew = easy_renumbering(S)
+    p = X.shape[1]
+    N = X.shape[0]
+    A = F.one_hot(S).type(torch.float32)
+    oneN = torch.ones(N, 1)
+    M = torch.mm(torch.mm(X.T, A), torch.diag(1/torch.mm(oneN.T, A).flatten()))
+    D = X.T - torch.mm(M, A.T)
+    P_w = 1/(P.max(dim = 1)[0])
     
-    for i in np.arange(num_clusters):
-        which = nodes[clustlabs == clust_IDs[i]]
-        clust = torch.index_select(X, 0, which)
-        centroid_mat[i,:] = torch.mean(clust, dim = 0)
-        pdist = nn.PairwiseDistance(p=norm_degree)
-        if weight_by == 'kmeans':
-            total_wcss += torch.mean(pdist(clust, centroid_mat[i,:].reshape(1, num_features)))
-        else:
-            total_wcss += torch.sum(pdist(clust, centroid_mat[i,:].reshape(1, num_features)))
-            
-    if weight_by == 'kmeans':
-        MSW = (1/num_clusters)*total_wcss
-    else:
-        MSW = (1/(X.shape[0]-num_clusters))*total_wcss
-            
-    return MSW, centroid_mat
+    MSW = (1/(N*k))*torch.sum(P_w * torch.diag(torch.mm(D.T, D)))
     
+    # M = torch.zeros(k, q)
+    # nodes = torch.arange(N)
+    # total_wcss = torch.zeros(1).float()
+    # clust_IDs = torch.unique(S)
+    
+    # for i in np.arange(k):
+    #     ix = nodes[S == clust_IDs[i]]
+    #     n_k = len(ix)
+    #     X_k = torch.index_select(X, 0, ix)
+    #     m_k = torch.index_select(X, 0, ix).mean(dim = 0)
+    #     M[i,:] = torch.mean(X_k, dim = 0)
+    #     #pdist = nn.PairwiseDistance(p=norm_degree)
+    #     total_wcss += (1/n_k)*torch.pow(torch.pow((X_k - m_k), norm_degree).sum(), 1/norm_degree)
+            
+    # if weight_by == 'kmeans':
+    #     MSW = (1/k)*total_wcss
+    # else:
+    #     MSW = (1/(N-k))*total_wcss
+            
+    return MSW, M
 
     
 
@@ -116,6 +154,14 @@ def resort_graph(A, sort_list):
 
 
 
+
+def easy_renumbering(labels):
+    clusts = torch.unique(labels)
+    new = torch.arange(len(clusts))
+    for index, (i,j) in enumerate(zip(clusts, new)):
+        labels[labels == int(i)] = int(j)
+        
+    return labels
 
 
 