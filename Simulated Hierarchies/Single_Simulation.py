db0242e4d62852941f3b882cd2203cfb56d18a98
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2024-02-06 15:40:32 -0800

    Update Single_Simulation.py
    a script to simulate a single network

--- Simulated Hierarchies/Single_Simulation.py
+++ Simulated Hierarchies/Single_Simulation.py
@@ -8,6 +8,8 @@ Created on Wed Jan 24 17:54:24 2024
 
 import random
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import argparse
 import numpy as np
 import networkx as nx
@@ -24,7 +26,7 @@ import pandas as pd
 sys.path.append('C:/Users/Bruin/Documents/GitHub/HGRN_repo/Simulated Hierarchies/')
 sys.path.append('C:/Users/Bruin/Documents/GitHub/HGRN_repo/HGRN_software/')
 from Simulate import simulate_graph
-from simulation_utilities import compute_graph_STATs
+from simulation_utilities import compute_graph_STATs, sort_labels
 #import os
 #os.chdir('C:/Users/Bruin/Documents/GitHub/HGRN_repo/Bethe Hessian Tests/')
 import warnings
@@ -35,6 +37,9 @@ import random as rd
 from itertools import product
 from tqdm import tqdm
 import time
+from sklearn.manifold import TSNE
+import pickle
+from sklearn.decomposition import PCA
 #set seed
 rd.seed(333)
 
@@ -55,7 +60,8 @@ parser.add_argument('--SD',dest='SD', default = 0.1, type=float)
 parser.add_argument('--common_dist', dest='common_dist',default = True, type=bool)
 parser.add_argument('--seed_number', dest='seed_number',default = 555, type=int)
 args = parser.parse_args()
-
+rd.seed(123)
+torch.manual_seed(123)
 
 # args.connect = 'full'
 # args.toplayer_connect_prob = 0.3
@@ -66,8 +72,80 @@ args.common_dist = True
 args.SD = 0.1
 args.node_degree = 3
 args.force_connect = True
-args.layers = 2,
+args.layers = 3
 args.connect = 'disc'
 args.subgraph_type = 'small world'
 args.savepath = 'C:/Users/Bruin/Documents/GitHub/HGRN_repo/Simulated Hierarchies/Toy_examples/Intermediate_examples/Results/test/'
-pe, gexp, nodes, edges, nx_all, adj_all, args.savepath, nodelabs = simulate_graph(args)
+pe, gexp, nodes, edges, nx_all, adj_all, args.savepath, nodelabs, orin = simulate_graph(args)
+
+
+
+indices_top, indices_mid, labels_df, sorted_true_labels_top, sorted_true_labels_middle = sort_labels(nodelabs)
+
+
+#C = F.one_hot(torch.Tensor(sorted_true_labels_top).to(torch.int64)).to(torch.float32)
+#X = torch.Tensor(pe_sorted).requires_grad_()
+origin_nodes = [i[0] for i in orin]
+if args.layers > 2:
+    pe_sorted = pe[indices_mid,:]
+    idx = [indices_mid.index(i) for i in origin_nodes]
+else:
+    pe_sorted = pe[indices_top,:]
+    idx = [indices_mid.index(i) for i in origin_nodes]
+
+labels = [sorted_true_labels_middle, sorted_true_labels_top]
+TSNE_embed=TSNE(n_components=3, 
+                learning_rate='auto',
+                init='random', 
+                perplexity=3).fit_transform(pe_sorted)
+PCs = PCA(n_components=3).fit_transform(pe_sorted)
+
+which_labels = ['Middle', 'Top']
+fig, ax = plt.subplots(2,2, figsize = (10,10))
+for i in range(0, len(labels)):
+    
+    ax[0][i].scatter(TSNE_embed[:,0], TSNE_embed[:,1], 
+                     s = 150.0, c = labels[i], cmap = 'plasma')
+    ax[0][i].scatter(TSNE_embed[idx,0], TSNE_embed[idx,1],
+                 c='red', s = 300, marker = '.')
+    ax[0][i].set_xlabel('Dimension 1')
+    ax[0][i].set_ylabel('Dimension 2')
+    ax[0][i].set_title( 't-SNE '+which_labels[i])
+    #adding node labels
+    ax[1][i].scatter(PCs[:,0], PCs[:,1], 
+                     s = 150.0, c = labels[i], cmap = 'plasma')
+    ax[1][i].scatter(PCs[idx,0], PCs[idx,1], 
+    c='red', s = 300, marker = '.')
+    ax[1][i].set_xlabel('Dimension 1')
+    ax[1][i].set_ylabel('Dimension 2')
+    ax[1][i].set_title( 'PCA '+which_labels[i])
+    
+    
+    
+    
+fig3d = plt.figure(figsize=(12,10))
+ax3d = plt.axes(projection='3d')
+ax3d.scatter3D(PCs[:,0], PCs[:,1], PCs[:,2], 
+              c=labels[1], cmap='plasma')
+ax3d.scatter3D(PCs[idx,0], PCs[idx,1], PCs[idx,2], 
+             c='red', s = 300, marker = '.',
+             depthshade = False)
+
+ax3d.set_xlabel('Dimension 1')
+ax3d.set_ylabel('Dimension 2')
+ax3d.set_zlabel('Dimension 3')
+
+
+
+
+fig3d = plt.figure(figsize=(12,10))
+ax3d = plt.axes(projection='3d')
+ax3d.scatter3D(TSNE_embed[:,0], TSNE_embed[:,1], TSNE_embed[:,2], 
+              c=labels[0], cmap='plasma')
+ax3d.scatter3D(TSNE_embed[idx,0], TSNE_embed[idx,1], TSNE_embed[idx,2], 
+             c='red', s = 300, marker = '.',
+             depthshade = False)
+
+ax3d.set_xlabel('Dimension 1')
+ax3d.set_ylabel('Dimension 2')
+ax3d.set_zlabel('Dimension 3')
